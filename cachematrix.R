4e727c1e40927b2746a39ff2f5f0a3cab92ef82b
armistead <johnawilson@gmail.com>
2016-09-01 22:49:07 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ cacheSolve <- function(x, ...) {
                 return(m)
         }
         data <- x$get()
-        m <- score(data, ...)
+        m <- solve(data, ...)
         x$setinv(m)
         m
 }
